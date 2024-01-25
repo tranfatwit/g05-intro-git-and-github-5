3639588e659f69c1a8309f82f2b288062c6c6643
ethanwu <wue1@wit.edu>
2024-01-25 11:20:38 -0500

    Add starter code

---
+++ src/MainApplication.java
@@ -0,0 +1,6 @@
+public class MainApplication {
+    public static void main(String[] args){
+        System.out.println("Mr.Cheddar wakes up and hears a knock on the door.");
+        System.out.println("Do you: \n[1]: Answer the Door. \n[2]: Ignore the Door. \n[E]: End game.");
+    }
+}
