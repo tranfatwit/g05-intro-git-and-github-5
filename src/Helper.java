4675aec62d1ce2def087468511c74e5c7de6cd07
Kristopher Bruno <brunok1@wit.edu>
2024-02-14 11:36:07 -0500

    Added Images (up until sewers)
    Images are held as Final Strings in the Images class, and are printed out when needed using a function in the Helper Class. There are still temporary images, and the rest need to be added.

---
+++ src/Helper.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+/***
+ * Utility method for MainApplication class
+ */
+public class Helper {
+    /***
+     * Method to format and print text for game story
+     * @param storyString string to print
+     */
+    public static void printStory(String storyString){
+        System.out.printf("\n%s\n", storyString);
+    }
+
+    /***
+     * Method to format and print text for game prompts
+     * @param promptString string to print
+     */
+    public static void printStoryPrompt(String promptString){
+        System.out.printf("\n%s", promptString);
+    }
+
+    public static void printStoryImage(String imageString){
+        System.out.printf("\n%s", imageString);
+    }
+
+    /***
+     * Method to grab and process a player's input
+     * @param input player's input
+     * @return integer to represent the player's choice
+     */
+    public static int getChoice(Scanner input){
+        String in = input.next();
+        try{
+            return Integer.parseInt(in);
+        } catch(NumberFormatException e){
+            System.out.println("You input an invalid option, exiting.");
+            System.exit(0);
+        }
+        return -1;
+    }
+
+    /***
+     * Method to display start menu and process player's selection
+     */
+    public static void startMenu () {
+        // Print start menu text
+        System.out.println();
+        System.out.println("##############");
+        System.out.println("#   [P]LAY   #");
+        System.out.println("#   [Q]UIT   #");
+        System.out.println("##############");
+        //printStoryPrompt("Enter choice: ");
+
+        // Grab user's input until a valid choice is entered
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String playChoice = input.next();
+            if(playChoice.equalsIgnoreCase("p")) {
+                // Exit loop if player wants to play
+                break;
+            } else if (playChoice.equalsIgnoreCase("q")) {
+                // Exit program if player wants to quit
+                System.out.println("Thanks for playing, take it cheesy!");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid input! Try again.");
+            }
+        }
+    }
+
+    /***
+     * Method to prompt player if they want to restart the game
+     */
+    public static void restartGamePrompt() {
+        printStoryPrompt("Do you want to play again?: \n[1]: Yes. \n[2]: No. \nEnter choice: ");
+        Scanner input = new Scanner(System.in);
+        if (Helper.getChoice(input) == 2) {
+            Helper.printStory("Thanks for playing, take it cheesy!");
+            System.exit(0);
+        }
+    }
+}
+
--- src/MainApplication.java
+++ src/MainApplication.java
@@ -46,6 +46,10 @@ public class MainApplication {
         printStoryPrompt("Do you: \n[1]: Answer the Door. \n[2]: Ignore the Door. \n[Other]: End game. \nEnter choice: ");
         
         choice = getChoice(input);
+=======
+            Images images = new Images();
+            /* Proceed to text adventure if player starts game */
+>>>>>>> Stashed changes
 
         switch (choice) {
             case 1:
@@ -62,6 +66,7 @@ public class MainApplication {
         
         choice = getChoice(input);
 
+<<<<<<< Updated upstream
         switch (choice) {
             case 2:
                 printStory("Mr. Cheese rushes out of the house and runs down to the police station.");
@@ -103,6 +108,138 @@ public class MainApplication {
                     printStoryPrompt("Do you: \n[1]: Interrupt her and head to the sewers. \n[2]: Let her keep talking. \n[Other]: End game. \nEnter choice: ");
                     
                     choice = getChoice(input);
+=======
+            switch (choice) {
+                case 1:
+                    Helper.printStory("Mr. Cheese gets out of bed.");
+                    break;
+                case 2:
+                {
+                    Helper.printStoryImage(images.Gorgonzola_HD);
+                    Helper.printStory("Mr. Gorgonzola picks the lock and kills you!");
+                }
+
+                default:
+                    // Exits if player wants to quit or continues to next iteration of loop if they want to restart
+                    Helper.restartGamePrompt();
+                    continue;
+            }
+
+            Helper.printStoryImage(images.Gorgonzola_HD);
+            Helper.printStory("He goes to the door and is met by a fellow worker at the spray cheese factory, Mr. Gorgonzola\n\"Mr. Cheese! Thank goodness you woke up! Your wife, Mrs. Cheddar, never showed up to the cheese tasting club, and she didn't pick up when we called her. I fear something terrible must have happened to her!\"");
+            Helper.printStoryPrompt("Do you: \n[1]: Let him inside to talk more. \n[2]: Go to the police station. \n[Other]: End game. \nEnter choice: ");
+
+            choice = Helper.getChoice(input);
+            
+
+            switch (choice) {
+                case 2:
+                    Helper.printStory("Mr. Cheese rushes out of the house and runs down to the police station.");
+                    break;
+                case 1:
+                {
+                    Helper.printStory("Mr. Gorgonzola pushes the door open and pulls out a knife and kills you. R.I.P!");
+                }
+
+                default:
+                    // Exits if player wants to quit or continues to next iteration of loop if they want to restart
+                    Helper.restartGamePrompt();
+                    continue;
+            }
+
+            Helper.printStoryImage(images.CheeseToPolice);
+            Helper.printStory("You enter the police station, and are greeted by the brightest police officer they have, Kurd Nerd Joe.");
+            Helper.printStoryPrompt("Do you: \n[1]: Search on your own. \n[2]: Let the police take over. \n[Other]: End game. \nEnter choice: ");
+
+            choice = Helper.getChoice(input);
+
+            switch (choice) {
+                case 1:
+                    Helper.printStory("Kurd Nerd Joe shows Mr. Cheese the CCTV camera where you see his wife heading towards the sewers. Mr. Cheese rushes out of the village and asks if anyone has seen his wife.");
+                    break;
+                case 2:
+                {
+                    Helper.printStoryImage(images.SadCheese);
+                    Helper.printStory("The police take over the investigation, but the case goes cold. Mr. Cheese never finds his wife.");
+                }
+                default:
+                    // Exits if player wants to quit or continues to next iteration of loop if they want to restart
+                    Helper.restartGamePrompt();
+                    continue;
+            }
+
+            Helper.printStoryImage(images.LeavePolice_Meeting);
+            Helper.printStory("On his way to the sewers, Miss Swiss approaches.");
+            Helper.printStoryPrompt("Do you: \n[1]: Tell her to 'Move out of my way!' \n[2]: Talk to her.\n[Other]: End game. \nEnter choice: ");
+
+            choice = Helper.getChoice(input);
+
+            switch (choice) {
+                case 1:
+                    Helper.printStory("Mr. Cheese pushes Miss Swiss aside, and heads towards the sewers.");
+                    break;
+                case 2:
+                    if (knifeFirst) {
+                        knife = true;
+                        Helper.printStoryImage(images.Knife);
+                        Helper.printStory("Miss Swiss gives Mr. Cheese a swiss army knife, and keeps talking...");
+                        Helper.printStoryPrompt("Do you: \n[1]: Interrupt her and head to the sewers. \n[2]: Let her keep talking. \n[Other]: End game. \nEnter choice: ");
+
+                        choice = Helper.getChoice(input);
+
+                        switch (choice) {
+                            case 1:
+                                Helper.printStoryImage(images.CheeseToSewer);
+                                Helper.printStory("Mr. Cheese says he has to go, and heads down to the sewers.");
+                                break;
+                            case 2:
+                                Helper.printStory("Miss Swiss continues talking and she accidentally blurted out to Mr. Cheese that she saw Mrs. Cheddar kissing Colby Jack.");
+                                knowledge = true;
+                                break;
+                            default:
+                                // Exits if player wants to quit or continues to next iteration of loop if they want to restart
+                                Helper.restartGamePrompt();
+                                continue;
+                        }
+                    } else {
+                        knowledge = true;
+                        Helper.printStory("She tells Mr. Cheese that she saw Mrs. Cheddar kissing Colby Jack.");
+                        Helper.printStoryPrompt("Do you: \n[1]: Interrupt her and head to the sewers \n[2]: Let her keep talking. \n[Other]: End game. \nEnter choice: ");
+
+                        choice = Helper.getChoice(input);
+
+                        switch (choice) {
+                            case 1:
+                                Helper.printStory("Mr. Cheese politely says he has to go, and heads down to the sewers.");
+                                break;
+                            case 2:
+                                Helper.printStoryImage(images.Knife);
+                                Helper.printStory("Miss Swiss gives Mr. Cheese a swiss army knife.");
+                                knife = true;
+                                break;
+                            default:
+                                // Exits if player wants to quit or continues to next iteration of loop if they want to restart
+                                Helper.restartGamePrompt();
+                                continue;
+                        }
+                    }
+                    break;
+                default:
+                    System.exit(0);
+            }
+
+            Helper.printStory("Enter Sewers");
+            Helper.printStoryPrompt("Do you: \n[1]: Go left. \n[2]: Go right. \n[Other]: End game. \nEnter choice: ");
+
+            choice = Helper.getChoice(input);
+
+            switch (Math.abs(choice - correctPath)) {
+                case 0:
+                    Helper.printStory("Mr. Cheese find Mrs. Cheddar holding a mouse trap up.");
+                    Helper.printStoryPrompt("Do you: \n[1] Help her, without risking yourself. \n[2] Hold it up yourself." + (knife ? " \n[" + curr++ + "] Use the knife to disarm the trap." : "") + (knowledge ? " \n[" + curr++ + "] Let her die for sleeping with Colby Jack." : "") + " \n[Other]: End game. \nEnter choice: ");
+
+                    choice = Helper.getChoice(input);
+>>>>>>> Stashed changes
 
                     switch (choice) {
                         case 1:
